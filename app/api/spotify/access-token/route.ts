0cc19eda38d80b2e9b7808996bc3719ef891bf29
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-07-23 14:19:27 +0300

    tried fixing window

--- app/api/spotify/access-token/route.ts
+++ app/api/spotify/access-token/route.ts
@@ -3,14 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 const client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 const client_secret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
+const inDev = process.env.NEXT_PUBLIC_IN_DEVELOPMENT;
 
 export async function POST(req: NextRequest) {
   const { code } = await req.json();
 
+  const redirect_uri =
+    inDev === "true"
+      ? "http://localhost:3000/spotify-auth"
+      : "https://mixine.vercel.app/spotify-auth";
+
   const params = new URLSearchParams();
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://localhost:3000/spotify-auth");
+  params.append("redirect_uri", redirect_uri);
 
   const authHeader = Buffer.from(`${client_id}:${client_secret}`).toString(
     "base64"
